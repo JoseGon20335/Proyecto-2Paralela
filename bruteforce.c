25a096e040652d321b0efb998e74388eea8b504a
Ldsc2002 <luissantoscuellar@gmail.com>
2023-10-13 20:24:27 -0600

    Code cleanup

--- bruteforce.c
+++ bruteforce.c
@@ -36,60 +36,58 @@ void myEncrypt(long key, char *ciph, int len) {
 
 char search[] = " es una prueba de ";
 int tryKey(long key, char *ciph, int len){
-  char temp[len+1];
-  memcpy(temp, ciph, len);
-  temp[len]=0;
-  myDecrypt(key, temp, len);
-  return strstr((char *)temp, search) != NULL;
+    char temp[len+1];
+    memcpy(temp, ciph, len);
+    temp[len]=0;
+    myDecrypt(key, temp, len);
+    return strstr((char *)temp, search) != NULL;
 }
 
 unsigned char cipher[] = {50, -12, -2, -82, -108, -75, -47, 45, 117, 110, 97, 32, 112, 114, 117, 101, 98, 97, 32, 100, 101, 32, 112, 114, 111, 121, 101, 99, 116, 111, 32, 50, 0};
 int main(int argc, char *argv[]){ //char **argv
-  int N, id;
-  long upper = (1L <<56); //upper bound DES keys 2^56
-  long mylower, myupper;
-  MPI_Status st;
-  MPI_Request req;
-  int flag;
-  int ciphlen = strlen(cipher);
-  MPI_Comm comm = MPI_COMM_WORLD;
+    int N, id;
+    long upper = (1L <<56); //upper bound DES keys 2^56
+    long mylower, myupper;
+    MPI_Status st;
+    MPI_Request req;
+    int flag;
+    int ciphlen = strlen(cipher);
+    MPI_Comm comm = MPI_COMM_WORLD;
 
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(comm, &N);
-  MPI_Comm_rank(comm, &id);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(comm, &N);
+    MPI_Comm_rank(comm, &id);
 
-  long range_per_node = upper / N;
-  mylower = range_per_node * id;
-  myupper = range_per_node * (id+1) -1;
-  if(id == N-1){
-    //compensar residuo
-    myupper = upper;
-  }
-
-  long found = 0;
-  int ready = 0;
-
-  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
-
-  for(long i = mylower; i<myupper; ++i){
-    MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
-    if(ready)
-      break;  //ya encontraron, salir
-
-    if(tryKey(i, (char *)cipher, ciphlen)){
-      found = i;
-      for(int node=0; node<N; node++){
-        MPI_Send(&found, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
-      }
-      break;
+    long range_per_node = upper / N;
+    mylower = range_per_node * id;
+    myupper = range_per_node * (id+1) -1;
+    if(id == N-1){
+        //compensar residuo
+        myupper = upper;
     }
-  }
 
-  if(id==0){
-    MPI_Wait(&req, &st);
-    myDecrypt(found, (char *)cipher, ciphlen);
-    printf("%li %s\n", found, cipher);
-  }
+    long found = 0;
+    int ready = 0;
 
-  MPI_Finalize();
+    MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+
+    for(long i = mylower; i<myupper; ++i){
+        MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
+        if(ready) break;  //ya encontraron, salir
+
+        if(tryKey(i, (char *)cipher, ciphlen)){
+            found = i;
+            for(int node=0; node<N; node++){
+                MPI_Send(&found, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
+            }
+        }
+    }
+
+    if(id==0){
+        MPI_Wait(&req, &st);
+        myDecrypt(found, (char *)cipher, ciphlen);
+        printf("%li - %s\n", found, cipher);
+    }
+
+    MPI_Finalize();
 }
